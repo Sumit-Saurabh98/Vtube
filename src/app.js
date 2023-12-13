ff83bf70a1284048ff6dcaa735819c2fa42035a4
Sumit-Saurabh98 <sumitsaurabh112@gmail.com>
2023-12-13 14:00:41 +0530

    all setup configure

--- src/app.js
+++ src/app.js
@@ -14,4 +14,11 @@ app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+import userRouter from "./routes/user.route.js"
+
+
+// routes declearation
+
+app.use("/api/v1/users", userRouter);
+
 export {app}
